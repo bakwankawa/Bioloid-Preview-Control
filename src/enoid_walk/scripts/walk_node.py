f7124efd2dcd9ef85d3fedee21f358eadf455b33
mahatma kawa <mahatmakawa@gmail.com>
2022-03-14 11:20:56 +0700

    Latest code from robot

--- src/enoid_walk/scripts/walk_node.py
+++ src/enoid_walk/scripts/walk_node.py
@@ -2,7 +2,7 @@
 
 import rospy
 from geometry_msgs.msg import Vector3, Twist
-from std_msgs.msg import Int32
+from std_msgs.msg import Int32, Bool
 
 from preview_controller import *
 from inverse_kinematic import *
@@ -18,6 +18,8 @@ gain_ctrl = Twist()
 feedback_mode = Int32()
 walk_mode = Int32()
 walk_cmd = Int32()
+push_data = Bool()
+com_msg = Vector3()
 
 FOOT_DISTANCE = 6.5 / 1000
 COM_HEIGHT = 230 / 1000
@@ -45,8 +47,7 @@ delta_step = 0.0
 
 def ori_callback(msg):
     ori_data[0] = msg.x
-    ori_data[1] = msg.y -  gain_ctrl.linear.x * np.pi /180
-    # print(ori_data[1]/np.pi*180)
+    ori_data[1] = msg.y 
 
 
 def gyr_callback(msg):
@@ -76,6 +77,9 @@ def gain_callback(msg):
     global gain_ctrl
     gain_ctrl = msg
 
+def push_callback(msg):
+    global push_data
+    push_data = msg
 
 def bezier_curve(phase, p_start, p_cnt, p_end):
     if(phase >= 1):
@@ -92,7 +96,7 @@ def bezier_curve(phase, p_start, p_cnt, p_end):
     return path
 
 def stand(ik):
-    global JOINTS, finish, step, delta_step, COM, LEFT, RIGHT
+    global JOINTS, finish, step, delta_step, COM, LEFT, RIGHT,  com_msg
     COM = np.matrix([X_OFFSET, 0.0, COM_HEIGHT])
     LEFT = np.matrix([0.0, 38.5 / 1000 + FOOT_DISTANCE, 0.0])
     RIGHT = np.matrix([0.0, -38.5 / 1000 - FOOT_DISTANCE, 0.0])
@@ -100,25 +104,37 @@ def stand(ik):
     finish = False
     step = False
     delta_step = 0.0
+    com_msg.x = COM[0,0]
+    com_msg.y = COM[0,1]
+    com_msg.z = COM[0,2]
+
 
 def walk_test(pc, ik):
-    global JOINTS, COM, LEFT, RIGHT
+    global JOINTS, COM, LEFT, RIGHT, com_msg
 
     pc.update_walking_pattern()
 
     if pc.walking_ready and not debug:
         JOINTS = ik.solve(pc.com_pose, pc.l_foot, pc.r_foot)
+        com_msg.x = pc.com_pose[0,0]
+        com_msg.y = pc.com_pose[0,1]
+        com_msg.z = pc.com_pose[0,2]
     else:
         JOINTS = ik.solve(COM, LEFT, RIGHT)
+    
+        com_msg.x = COM[0,0]
+        com_msg.y = COM[0,1]
+        com_msg.z = COM[0,2]
+
 
 def push_test(fz, ik):
-    global flag, finish, delta_step,step, start, now, JOINTS, COM, LEFT, RIGHT, uCOM, uLEFT, uRIGHT
+    global flag, finish, delta_step,step, start, now, JOINTS, COM, LEFT, RIGHT,uCOM, uLEFT, uRIGHT, com_msg
     time_step = .1
 
     if finish == False:
 
-        if (ori_data[1] * 180 / np.pi >= -20 and gyr_data[1] * 180 / np.pi <= 20) and not(step):
-            print("IN 1")
+        if not(push_data.data) and not(step):
+            # print("IN 1")
             JOINTS = ik.solve(COM, LEFT, RIGHT)
             start = rospy.Time.now().to_sec()
             delta_step = fz.compute(-ori_data[1] * 180 /
@@ -126,9 +142,9 @@ def push_test(fz, ik):
             uCOM = COM
             uLEFT = LEFT
             uRIGHT = RIGHT
-
+            # delta_step = 0.07
         elif not(finish):
-            print("STEP")
+            # print("STEP")
             now = rospy.Time.now().to_sec() - start
             phase = now / time_step
             
@@ -137,7 +153,7 @@ def push_test(fz, ik):
                 finish = True
 
             COM = bezier_curve(phase, uCOM, np.matrix(
-                [X_OFFSET + (delta_step / 4.0), -0.03, COM_HEIGHT]), np.matrix([X_OFFSET + (delta_step / 2.0), 0.01, COM_HEIGHT]))
+                [X_OFFSET + (delta_step / 4.0), -0.03, COM_HEIGHT]), np.matrix([X_OFFSET + (delta_step / 2.0), -0.005, COM_HEIGHT]))
 
             if True:
                 LEFT = bezier_curve(phase, uLEFT, np.matrix(
@@ -150,15 +166,19 @@ def push_test(fz, ik):
 
             step = True
 
+        com_msg.x = COM[0,0]
+        com_msg.y = COM[0,1]
+        com_msg.z = COM[0,2]
+
     
 def final_test(pc, fz, ik):
-    global flag, finish, delta_step,step, start, now, JOINTS, COM, LEFT, RIGHT, uCOM, uLEFT, uRIGHT
+    global flag, finish, delta_step,step, start, now, JOINTS, COM, LEFT, RIGHT, uCOM, uLEFT, uRIGHT, push_data, com_msg
     time_step = .1
     pc.update_walking_pattern()
     if finish == False:
         if pc.walking_ready and not debug:
-            if (ori_data[1] * 180 / np.pi >= -35 and gyr_data[1] * 180 / np.pi <= 33) and not(step):
-                print("WALK")
+            if not(push_data.data) and not(step):
+                # print("WALK")
                 JOINTS = ik.solve(pc.com_pose, pc.l_foot, pc.r_foot)
                 start = rospy.Time.now().to_sec()
                 delta_step = fz.compute(-ori_data[1] * 180 /
@@ -166,8 +186,13 @@ def final_test(pc, fz, ik):
                 uCOM = pc.com_pose
                 uLEFT = pc.l_foot
                 uRIGHT = pc.r_foot
+
+                com_msg.x = uCOM[0,0]
+                com_msg.y = uCOM[0,1]
+                com_msg.z = uCOM[0,2]
+
             elif not(finish):
-                print("STEP")
+                # print("STEP")
                 now = rospy.Time.now().to_sec() - start
                 phase = now / time_step
                 
@@ -180,19 +205,26 @@ def final_test(pc, fz, ik):
 
                 if pc.support_foot == -1:
                     LEFT = bezier_curve(phase, uLEFT, np.matrix(
-                        [(delta_step / 2.0),  48/1000, 0.08]), np.matrix([(delta_step),  58/1000, 0.0]))
+                        [(delta_step / 2.0),  48/1000, 0.08]), np.matrix([(delta_step),  62/1000, 0.0]))
                 else:
                     RIGHT = bezier_curve(phase, uRIGHT, np.matrix(
-                        [(delta_step / 2.0), -48/1000, 0.08]), np.matrix([(delta_step), -58/1000, 0.0]))
+                        [(delta_step / 2.0), -48/1000, 0.08]), np.matrix([(delta_step), -62/1000, 0.0]))
 
                 JOINTS = ik.solve(COM, LEFT, RIGHT)
 
                 step = True
+
+                com_msg.x = COM[0,0]
+                com_msg.y = COM[0,1]
+                com_msg.z = COM[0,2]
         else:
             JOINTS = ik.solve(COM, LEFT, RIGHT)
+            com_msg.x = COM[0,0]
+            com_msg.y = COM[0,1]
+            com_msg.z = COM[0,2]
 
 def main():
-    global X_OFFSET
+    global X_OFFSET, COM, com_msg
     rospy.init_node('enoid_walk', anonymous=False)
     rospy.Subscriber("ori_data", Vector3, ori_callback)
     rospy.Subscriber("gyr_data", Vector3, gyr_callback)
@@ -201,6 +233,8 @@ def main():
     rospy.Subscriber("walk_mode_status", Int32, walk_mode_callback)
     rospy.Subscriber("feedback_status", Int32, feedback_callback)
     rospy.Subscriber("gain_control", Twist, gain_callback)
+    rospy.Subscriber("push_data", Bool, push_callback)
+    com_pub = rospy.Publisher('com_data', Vector3, queue_size=1)
 
     rate = rospy.Rate(30)
 
@@ -217,7 +251,7 @@ def main():
         
         if walk_cmd.data == -1:
 
-            print("RESET WALK")
+            # print("RESET WALK")
             pc = PreviewControl(COM_HEIGHT, COM_SWING, X_OFFSET, FOOT_DISTANCE)
             stand(ik)
 
@@ -241,21 +275,22 @@ def main():
 
                 
             if ((feedback_mode.data == 1) or (feedback_mode.data == 2)) and not(finish):
-                print(ori_data[1] * 180/np.pi)
-                print(gyr_data[1] * 180/np.pi)
+                # print(ori_data[1] * 180/np.pi)
+                # print(gyr_data[1] * 180/np.pi)
                 
-                print("ANKLE")
+                # print("ANKLE")
                 igl_data[1] +=  ori_data[1]/30
 
-                delta_roll = gain_ctrl.linear.x * ori_data[0] + gain_ctrl.angular.x * -gyr_data[0]
-                delta_pitch = -gain_ctrl.linear.y * ori_data[1] + gain_ctrl.angular.y * gyr_data[1] + gain_ctrl.angular.x * igl_data[1]
+                delta_roll = 0.04 * ori_data[0] + 0.036 * gyr_data[0]
+                delta_pitch = -gain_ctrl.linear.y * ori_data[1] + -gain_ctrl.angular.y * gyr_data[1] + gain_ctrl.angular.x * igl_data[1]
                 JOINTS[3] += delta_pitch
                 JOINTS[8] -= delta_pitch
                 # JOINTS[4] += delta_roll
-                # JOINTS[9] += delta_roll
+                JOINTS[9] -= delta_roll
 
         sc.sync_write_pos(JOINTS)
 
+        com_pub.publish(com_msg)
         rate.sleep()
 
 if __name__ == '__main__':
